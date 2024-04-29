ea95a318cdc82dd7778e2429a8e9c394d17e7164
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2024-04-29 11:48:24 +0100

    reducing LCP

--- page-templates/blocks/cb_hero_full.php
+++ page-templates/blocks/cb_hero_full.php
@@ -2,15 +2,16 @@
 
 $link = get_field('cta') ?? null;
 
-$img = wp_get_attachment_image_url(get_field('background'),'full') ?? null;
+$img = wp_get_attachment_image(get_field('background'),'full',false, array('class' => 'hero_full__bg','loading' => 'lazy')) ?? null;
 
 ?>
-<link rel="preload" href="<?=$img?>" as="image">
-<section class="hero_full" style="background-image:url(<?=$img?>)">
+<!-- <link rel="preload" href="<?=$img?>" as="image"> -->
+<section class="hero_full">
+    <?=$img?>
     <div class="hero_full__content container-xl text-center">
         <h1 class="text-white"><?=get_field('title')?></h1>
         <?php
-        if (get_field('subtitle') ?? null) {
+        if (get_field('subtitle') ?? null) {    
             ?>
         <div class="hero_full__subtitle">
             <?=get_field('subtitle')?>
